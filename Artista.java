f9753df8cbef0c4c4249ad636f70b24973e9c56e
Lucia <lucia.galiano.nieto.alu@iesfernandoaguilar.es>
2024-03-18 13:26:59 +0100

    Modificación artista.java

---
+++ Artista.java
@@ -0,0 +1,25 @@
+package evf.javadoc.original;
+
+public class Artista {
+    String nombreArtista;
+	//
+    public Artista(String nombreArtista) {
+	//Hola
+        this.nombreArtista = nombreArtista;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "nombreArtista='" + nombreArtista + '\'' +
+                '}';
+    }
+
+    public String getNombreArtista() {
+        return nombreArtista;
+	//Esto es un comentario
+    }
+    
+}
+
+
